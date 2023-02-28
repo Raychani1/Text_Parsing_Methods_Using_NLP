8a61cf5a4c11bbd2546581dd0fbf09a66b286738
Raychani1 <rajcsanyi.ladislav.it@gmail.com>
2023-02-28 07:52:59 +0100

    Change model to slovakbert-ner

--- text_parsing_methods_using_nlp/annotation/annotator.py
+++ text_parsing_methods_using_nlp/annotation/annotator.py
@@ -1,7 +1,5 @@
 import csv
 import os
-import string
-from pprint import pprint
 
 import pandas as pd
 from transformers import pipeline
@@ -19,12 +17,11 @@ class Annotator:
         )
     ) -> None:
         # TODO - Docstring
-        self._data = pd.read_csv(input_data_filepath, delimiter=',')
+        self._data = pd.read_csv(
+            input_data_filepath, delimiter=',', encoding='utf-8')
         self._ner_pipeline = pipeline(
             task='ner',
-            # model='crabz/slovakbert-ner'
-            model='crabz/fernet-cc_sk-ner'
-
+            model='crabz/slovakbert-ner'
         )
         self._output_path = (
             f"{input_data_filepath.split('.')[0]}_annotated.csv"
@@ -34,26 +31,48 @@ class Annotator:
         # TODO - Docstring
 
         regex_mapping = {
-            # Replace floating point number commas with dot notation
-            r'([+-]?[0-9]+),([0-9]+)': r'\1.\2',
-
-            # Add extra space to floating point percentages
-            r'([+-]?[0-9]+.[0-9]+|[+-]?[0-9])(%)': r'\1 \2',
 
             # Add an extra space before colon which is not a time indicator
             r'([a-zA-Z]+):([a-zA-Z]+)': r'\1 : \2',
 
+            r'(\[\s*[0-9]+\s*\])': r' ',
+
             # Remove special characters
-            r'''['"`„“\(\)\/(\s\-|–\s))]|(: )''': r' ',  # add , if needed
+            r'''['"`‘’„“”\(\)\[\]\/(\s\-|–\s))!?;]|(\s*:\s+)|(\.|,)\s*(\.|,)''': r' ',
 
             # Remove dots from text
-            r'([a-zA-Z]{2,})(\.\s+)([a-zA-Z]+)': r'\1 \3',
+            r'([a-zA-Z]{2,})(\s*\.\s*)': r'\1 ',
+
+            # Remove dots from text
+            r'([ľščťžýáíéóúäôňďĺ%]{1,}[a-zA-Z0-9]*)\s*(\.)\s*': r'\1 ',
+
+            # Remove dots from text
+            r'([0-9]{1,})(\s*\.\s*)': r'\1 ',
+
+            # Replace floating point number commas with dot notation
+            r'([+-]?[0-9]+),([0-9]+)': r'\1.\2',
+
+            # Add extra space to floating point percentages
+            r'([+-]?[0-9]+.[0-9]+|[+-]?[0-9])(%)': r'\1 \2 ',
+
+            r',': r' ',
+
+            # # Remove dots from text
+            # r'([a-zA-Z]{2,})(\.\s*)([a-zA-Z]*)': r'\1 \3',
 
             # Replace excessive whitespace characters
             r'\s+': r' ',
 
             # Merge larger number formats together
-            r'( [+-]?[0-9]{1,3}) ([0-9]+) ([a-z]+)': r'\1\2 \3'
+            r'( [+-]?[0-9]{1,3}) ([0-9]+) ([a-z]*)': r'\1\2 \3',
+
+            # r'j\.\s*s\.\s*a': 'j.s.a',
+
+            # r'a\.\s*s\.': 'a.s.',
+
+            # r'atď .': 'atď.',
+
+            r'(\s+[a-zA-Zľščťžýáíéóúäôňďĺ]) \.': r'\1.',
         }
 
         for to_replace, replacement in regex_mapping.items():
@@ -82,68 +101,47 @@ class Annotator:
         self._preprocess_data()
         self._save_data()
 
-        for i in range(len(self._data)):
+        for i in range(500):
+
             classifications = self._ner_pipeline(self._data['text'][i])
 
             tokens = self._data['lemma_text'][i].split()
-            words = []
+            text_tokens = self._data['text'][i].split()
+            # words = []
             ner_tags = []
-            current_words = []
+            # current_words = []
             current_ner_tag = []
 
             for index, classification in enumerate(classifications):
-                current_words.append(classification['word'].replace('#', ''))
-
+                # current_words.append(classification['word'].replace('Ġ', ''))
                 current_ner_tag.append(classification['entity'])
-
-                current_words = [''.join(current_words)]
+                # current_words = [''.join(current_words)]
 
                 if len(set(current_ner_tag)) == 1:
                     current_ner_tag = [current_ner_tag[0]]
 
                 if (
-                    index < len(classifications) - 1 and (
-                        classifications[index + 1]['word'].startswith('#')
+                    index < len(classifications) - 1 and
+                    not classifications[index + 1]['word'].startswith(
+                        tuple(['Ġ', ','])
                     )
                 ):
                     continue
-
-                if (
-                    index < len(classifications) - 2
-                    and (
-                        (
-                            (classification['word'].isdigit()
-                             or len(classification['word']) == 1)
-                            and classifications[index + 1]['word'].startswith(
-                                tuple(['.', ':'])  # comma
-                            )
-                        )
-                        or (
-                            classification['word'].startswith(
-                                tuple(['.', ':'])  # comma
-                            )
-                            and classifications[index + 1]['word'].isdigit()
-                        )
-                    )
-                ):
-                    continue
-
-                print(current_words)
-                print(current_ner_tag)
 
                 ner_tags.extend(current_ner_tag) if len(
                     current_ner_tag) == 1 else ner_tags.append(current_ner_tag)
-                words.extend(current_words)
+                # words.extend(current_words)
 
-                current_words = []
+                # current_words = []
                 current_ner_tag = []
 
-            print(f'Our: {words}')
-            print(f'Official: {tokens}')
-            print(ner_tags)
+            # print(f'Our: {words}')
+            # print(f'Official Text: {text_tokens}')
+            # print(f'Official Lemma: {tokens}')
+            # print(ner_tags)
 
-            print(f'Same Length: {len(tokens) == len(ner_tags)}')
-            print(len(tokens))
-            print(len(ner_tags), end='\n\n')
-
-            # self._save_data()
+            # print(len(ner_tags))
+            # print(f'Same Length: {len(tokens) == len(ner_tags)}')
+            # print(f'Our: {len(words)}')
+            # print(f'Official Text: {len(text_tokens)}')
+            # print(f'Official Lemma: {len(tokens)}', end='\n\n')
