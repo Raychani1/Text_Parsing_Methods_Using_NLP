9b78bb98542bd2e742b3fed3be3220cd1a4598f3
Raychani1 <rajcsanyi.ladislav.it@gmail.com>
2023-03-05 19:10:00 +0100

    Add docstrings

--- main.py
+++ main.py
@@ -4,9 +4,17 @@ from text_parsing_methods_using_nlp.annotation.annotator import Annotator
 
 
 if __name__ == '__main__':
+    dataset_size = 100
+
     annotator = Annotator(
-        dataset_size=100,
-        manual_correction_filepath=os.path.join(os.getcwd(), 'manual.txt')
+        dataset_size=dataset_size,
+        manual_correction_filepath=os.path.join(
+            os.getcwd(),
+            'text_parsing_methods_using_nlp',
+            'data',
+            'manual_correction_files',
+            f'manual_{dataset_size}.txt'
+        )
     )
 
     annotator()
