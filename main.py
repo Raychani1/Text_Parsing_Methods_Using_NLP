004ed920d3420842778a32b206848faf74fd1a16
Raychani1 <rajcsanyi.ladislav.it@gmail.com>
2023-03-18 14:18:53 +0100

    Automate variable length annotation

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from text_parsing_methods_using_nlp.annotation.annotator import Annotator
 
 
 if __name__ == '__main__':
-    dataset_size = 100
 
-    annotator = Annotator(dataset_size=dataset_size)
+    for i in range(100, 451, 50):
+        annotator = Annotator(dataset_size=i)
 
-    annotator()
+        annotator()
+
+        del annotator
