bc926ae9028a9f06f6e287a9d92fe69424e0edc7
Raychani1 <rajcsanyi.ladislav.it@gmail.com>
2023-04-09 16:14:01 +0200

    Refactor main script

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import os
 
-from text_parsing_methods_using_nlp.annotation.annotator import Annotator
-from text_parsing_methods_using_nlp.models.slovakbert_ner_model import (
-    SlovakBertNerModel
-)
+import text_parsing_methods_using_nlp as tpm_nlp
+
+
+def run_annotation() -> None:
+    for i in range(100, 451, 50):
+        annotator = tpm_nlp.Annotator(dataset_size=i)
+
+        annotator()
+
+        del annotator
+
+
+def run_modeling() -> None:
+    for v in ['0.0.4']:
+        model = tpm_nlp.SlovakBertNerModel(version=v)
+
+        model()
+
+        del model
+
 
 if __name__ == '__main__':
-
-    # # Annotation
-    # for i in range(100, 451, 50):
-    #     annotator = Annotator(dataset_size=i)
-
-    #     annotator()
-
-    #     del annotator
-
-    # Model
-    model = SlovakBertNerModel()
-
-    model()
+    run_modeling()
