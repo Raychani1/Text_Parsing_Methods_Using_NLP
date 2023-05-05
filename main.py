9797c139c21323e8f58e2fcc3b6a77cb963a9f02
Raychani1 <rajcsanyi.ladislav.it@gmail.com>
2023-05-05 23:47:42 +0200

    Add minimal use-case methods

--- main.py
+++ main.py
@@ -1,115 +1,194 @@
 import itertools
 import os
+from typing import Any, Dict, List
 
-import spacy
-from spacy import displacy
-
+import pandas as pd
 from natsort import natsorted
 from transformers import (
     pipeline,
     AutoModelForTokenClassification,
     AutoTokenizer,
-    IntervalStrategy
 )
 
 import text_parsing_methods_using_nlp as tpm_nlp
-from text_parsing_methods_using_nlp.config import (
+from text_parsing_methods_using_nlp.config.config import (
     MODEL_OUTPUT_FOLDER,
     MODEL_TEST_DATASET_FOLDER,
-    INVERTED_NER_LABELS
+)
+from text_parsing_methods_using_nlp.config.model_config import (
+    MODEL_CONFIG_V_POC,
+    MODEL_CONFIG_V0_0_1,
+    MODEL_CONFIG_V0_0_2,
+    MODEL_CONFIG_V0_0_3,
+    MODEL_CONFIG_V0_0_4,
+    MODEL_CONFIG_V0_0_5,
+    MODEL_CONFIG_V0_0_6
 )
 
-from pprint import pprint
 
-# TODO - Previous version configurations
+def run_annotation(model_name: str, model_path: str) -> None:
+    """Executes annotation process with specific model.
 
-MODEL_CONFIG_V_POC = {
-    'version': '_POC',
-    'hyperparameter_tuning':  False,
-    'concat_with_wikiann': False,
-    'do_train': True,
-    'do_eval': True,
-    'do_predict': True,
-    'evaluation_strategy': IntervalStrategy.EPOCH,
-    'per_device_train_batch_size': 4,
-    'per_device_eval_batch_size': 4,
-    'num_train_epochs': 15,
-    'logging_strategy': IntervalStrategy.EPOCH,
-    'save_strategy': IntervalStrategy.EPOCH,
-    'save_total_limit': 2,
-    'eval_steps': 500,
-    'report_to': 'wandb',
-    'load_best_model_at_end': True,
-    'metric_for_best_model': 'f1',
-}
+    Args:
+        `model_name` (str): Model name to use in annotation process evaluation 
+        plots.
+        `model_path` (str): Path to model to use in annotation process.
+    """
+    annotator = tpm_nlp.Annotator(
+        model=model_path,
+        model_name=model_name,
+        tokenizer=model_path,
+    )
+
+    annotator()
 
 
-MODEL_CONFIG_V0_0_5 = {
-    'version': '0.0.5',
-    'early_stopping_patience': 3,
-    'hyperparameter_tuning':  False,
-    'do_train': True,
-    'do_eval': True,
-    'do_predict': True,
-    'evaluation_strategy': IntervalStrategy.EPOCH,
-    'per_device_train_batch_size': 4,
-    'per_device_eval_batch_size': 4,
-    'learning_rate': 0.000024981604754,
-    'weight_decay': 0.285214291922975,
-    'num_train_epochs': 15,
-    'logging_strategy': IntervalStrategy.EPOCH,
-    'save_strategy': IntervalStrategy.EPOCH,
-    'save_total_limit': 2,
-    'eval_steps': 500,
-    'report_to': 'wandb',
-    'load_best_model_at_end': True,
-    'metric_for_best_model': 'f1',
-}
+# region Modeling
 
 
-def run_annotation() -> None:
-    for i in range(100, 451, 50):
-        annotator = tpm_nlp.Annotator(dataset_size=i)
+def run_model_training(config: Dict[str, Any]) -> None:
+    """Executes model training based on configurations.
 
-        annotator()
+    Args:
+        `config` (Dict[str, Any]): Model configurations to be used for
+        training.
+    """
+    model = tpm_nlp.SlovakBertNerModel(**config)
+    model()
 
-        del annotator
+    del model
+
+
+def run_early_stopping_training(
+    config: Dict[str, Any],
+    patience_values: List[int]
+) -> None:
+    """Executes Early Stopping Training.
+
+    Args:
+        `config` (Dict[str, Any]): Model configurations to be used for
+        training.
+        `patience_values` (List[int]): Patience values for training.
+    """
+    for patience in patience_values:
+        config['early_stopping_patience'] = patience
+
+        run_model_training(config)
+
+
+def run_hyperparam_tuning_training(
+    config: Dict[str, Any],
+    number_of_iterations: int
+) -> None:
+    """Executes Hyperparameter Tuning Training.
+
+    Args:
+        `config` (Dict[str, Any]): Model configurations to be used for
+        training.
+        `number_of_iterations` (int): Number of iterations to repeat training.
+    """
+    for _ in range(number_of_iterations):
+        run_model_training(config)
+
+
+def run_dropout_training(
+    config: Dict[str, Any],
+    dropout_values: List[int]
+) -> None:
+    """Executes Dropout Value Modification Training.
+
+    Args:
+        `config` (Dict[str, Any]): Model configurations to be used for
+        training.
+        `dropout_values` (List[int]): Dropout values for training.
+    """
+    for (
+        hidden_dropout_prob,
+        attention_probs_dropout_prob,
+        classifier_dropout_value
+    ) in [
+        p for p in itertools.product(
+            dropout_values,
+            repeat=3
+        )
+    ]:
+        config['hidden_dropout_prob'] = hidden_dropout_prob
+        config['attention_probs_dropout_prob'] = (
+            attention_probs_dropout_prob
+        )
+        config['classifier_dropout_value'] = (
+            classifier_dropout_value
+        )
+
+        run_model_training(config)
+
+
+def run_freezing_training(config: Dict[str, Any]) -> None:
+    """Executes Layer Freezing Training.
+
+    Args:
+        `config` (Dict[str, Any]): Model configurations to be used for
+        training.
+    """
+    for freezing in [
+        [],
+        range(0, 1),
+        range(0, 3),
+        range(0, 5),
+        range(0, 7),
+        range(0, 10),
+        range(0, 12),
+    ]:
+        layers_to_freeze = ['embeddings']
+        layers_to_freeze.extend(freezing)
+
+        config['layers_to_freeze'] = layers_to_freeze
+
+        run_model_training(config)
 
 
 def run_modeling() -> None:
-    # V0.0.5
-    # for (
-    #     hidden_dropout_prob,
-    #     attention_probs_dropout_prob,
-    #     classifier_dropout_value
-    # ) in [
-    #     p for p in itertools.product(
-    #         [0.15, 0.2, 0.25],
-    #         repeat=3
-    #     )
-    # ]:
-    #     MODEL_CONFIG_V0_0_5['hidden_dropout_prob'] = hidden_dropout_prob
-    #     MODEL_CONFIG_V0_0_5['attention_probs_dropout_prob'] = (
-    #         attention_probs_dropout_prob
-    #     )
-    #     MODEL_CONFIG_V0_0_5['classifier_dropout_value'] = (
-    #         classifier_dropout_value
-    #     )
+    """Executes Every Modeling Training."""
+    for config in [
+        MODEL_CONFIG_V_POC,
+        MODEL_CONFIG_V0_0_1,
+        MODEL_CONFIG_V0_0_2,
+        MODEL_CONFIG_V0_0_3,
+        MODEL_CONFIG_V0_0_4,
+        MODEL_CONFIG_V0_0_5,
+        MODEL_CONFIG_V0_0_6
+    ]:
+        if config['version'] == '0.0.3':
+            run_early_stopping_training(config=config, patience_values=[3, 10])
 
-    #     model = tpm_nlp.SlovakBertNerModel(**MODEL_CONFIG_V0_0_5)
-    #     model()
+        elif config['version'] == '0.0.4':
+            run_hyperparam_tuning_training(
+                config=config,
+                number_of_iterations=10
+            )
 
-    #     del model
+        elif config['version'] == '0.0.5':
+            run_dropout_training(
+                config=config,
+                dropout_values=[0.15, 0.2, 0.25]
+            )
 
-    # V POC
-    model = tpm_nlp.SlovakBertNerModel(**MODEL_CONFIG_V_POC)
-    model()
+        elif config['version'] == '0.0.6':
+            run_freezing_training(config=config)
+
+        else:
+            run_model_training(config=config)
+
+# endregion
 
 
-def run_evaluation() -> None:
-    # TODO - Docstring
+def run_evaluation(versions: List[str]) -> None:
+    """Runs evaluation for every passed model version.
 
-    for version in ['_POC', '0.0.1', '0.0.2', '0.0.3', '0.0.4', '0.0.5']:
+    Args:
+        `versions` (List[str]): Model versions to evaluate.
+    """
+    for version in versions:
         version_folder = [
             s for s in os.listdir(MODEL_OUTPUT_FOLDER) if version in s
         ][0]
@@ -143,14 +222,20 @@ def run_evaluation() -> None:
                 model_input_folder=os.path.join(
                     model_version_folder,
                     model_name
-                )
+                ),
+                plot_history=True
             )
 
             del model
 
 
-def run_prediction(model_path):
-    # TODO - Docstring
+def run_prediction(model_path: str) -> None:
+    """Executes prediction with a specific model.
+
+    Args:
+        `model_path` (str): Path to model used for prediction.
+    """
+    input_sentence = input('Please provide Input to predict: ')
 
     ner_pipeline = pipeline(
         'ner',
@@ -158,79 +243,23 @@ def run_prediction(model_path):
         tokenizer=AutoTokenizer.from_pretrained(model_path)
     )
 
-    input_sentence = input('Please provide Input to predict: ')
-
     classifications = ner_pipeline(input_sentence)
 
-    entities = []
-    for i in range(len(classifications)):
-        if classifications[i]['entity'] != '0':
-            print(classifications[i]['entity'])
-
-            if classifications[i]['entity'][0] == 'B':
-                j = i + 1
-                while j < len(classifications) and classifications[i]['entity'].split('-')[-1] == classifications[j]['entity'].split('-')[-1]:
-                    j += 1
-                entities.append(
-                    (
-                        classifications[i]['entity'].split('-')[-1], 
-                        classifications[i]['start'],
-                        classifications[j - 1]['end']
-                    )
-                )
-
-    nlp = spacy.blank("sk")  # it should work with any language
-
-    doc = nlp(input_sentence)
-
-    ents = []
-    for ee in entities:
-        print(ee)
-
-        word = doc.char_span(ee[1], ee[2], ee[0])
-
-        if not ents:
-            previous_range = range(-1, -1)
-
-        current_range = range(ee[1], ee[2])
-              
-
-        if word and not (
-            current_range.start in previous_range and current_range[-1] in previous_range
-        ):
-            ents.append(word)
-
-        previous_range = current_range
-
-    doc.ents = ents
-
-    options = {
-        "ents": [
-            "PERSON",
-            "ORGANIZATION",
-            "LOCATION",
-            'DATE',
-            'TIME',
-            'MONEY',
-            'PERCENTAGE'
-        ],
-        "colors": {
-            "PERSON": "lightblue",
-            "ORGANIZATION": "lightcoral",
-            "LOCATION": "#FFB703",
-            'DATE': "#EF233C",
-            'TIME': "#CCFF33",
-            'MONEY': "lightgreen",
-            'PERCENTAGE': "#8D99AE"
-        }
-    }
-    displacy.serve(doc, style="ent", options=options, auto_select_port=True)
+    tpm_nlp.display_ner_classification(
+        input_sentence=input_sentence,
+        classifications=classifications
+    )
 
 
 if __name__ == '__main__':
-    # run_modeling()
-    # run_evaluation()
+    run_annotation(model_name='my-model', model_path='path/to/my-model')
 
-    run_prediction(
-        model_path='MODEL_PATH'
+    run_modeling()
+
+    run_evaluation(
+        versions=[
+            '_POC', '0.0.1', '0.0.2', '0.0.3', '0.0.4', '0.0.5', '0.0.6'
+        ]
     )
+
+    run_prediction(model_path='path/to/my-model')
