d52199e773242cca319c36f91739b518f2e4d124
Raychani1 <rajcsanyi.ladislav.it@gmail.com>
2023-05-05 23:48:09 +0200

    Add new method to __init__.py

--- text_parsing_methods_using_nlp/__init__.py
+++ text_parsing_methods_using_nlp/__init__.py
@@ -2,3 +2,6 @@ from text_parsing_methods_using_nlp.annotation.annotator import Annotator
 from text_parsing_methods_using_nlp.models.slovakbert_ner_model import (
     SlovakBertNerModel
 )
+from text_parsing_methods_using_nlp.utils.utils import (
+    display_ner_classification
+)
